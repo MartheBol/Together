b41b22cb5e913b292a276affb1cb0b35b3cd2ed5
Iman Cocquyt <iman.cocquyt@student.howest.be>
2015-12-05 23:51:22 +0100

    Matches - Iman (aanpassing)

---
+++ Aanpassing Iman - Matches/src/app.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Marthe on 1/12/15.
+ */
+(function () {
+    "use strict";
+
+    var app = angular.module("app", ["ngRoute"]);
+    app.config(function ($routeProvider) {
+        //als de pagina eindigt op all dan {} uitvoeren
+        $routeProvider
+            .when("/search", {
+                templateUrl: "ImagesOverview/search.html"
+            })
+            .when("/details/:id", {
+                templateUrl: "ImagesDetails/details.html"
+            })
+            .when("/home", {
+                templateUrl: "home/home.html"
+            })
+            .when("/howto", {
+                templateUrl: "howTo/howTo.html"
+            })
+            .when("/register", {
+                templateUrl: "profile/register.html"
+            })
+            .when("/matches", {
+                templateUrl: "matches/matches.html"
+            })
+
+            .otherwise({
+                redirectTo: "/home"
+            });
+    });
+
+})();
