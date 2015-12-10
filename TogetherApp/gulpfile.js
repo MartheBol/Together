d1b78ba196057ed45dd4e4beea7ac15ef8e2aa7b
MartheBol <Marthe@MacBook-Pro-van-Marthe.local>
2015-12-10 20:46:32 +0100

    gulpfile aangepast aan structuur

--- TogetherApp/gulpfile.js
+++ TogetherApp/gulpfile.js
@@ -2,48 +2,50 @@
  * Created by Marthe on 28/11/15.
  */
 var gulp = require("gulp"),
-  csslint = require("gulp-csslint"),
-  cssMinifier = require("gulp-minify-css"),
-  sourcemaps = require("gulp-sourcemaps"),
-  concat = require("gulp-concat"),
-  less = require("gulp-less"),
-  notify = require("gulp-notify"),
-  uglify = require("gulp-uglify"),
-  jshint = require("gulp-jshint"),
-  jsStylish = require("jshint-stylish");
+    csslint = require("gulp-csslint"),
+    cssMinifier = require("gulp-minify-css"),
+    sourcemaps = require("gulp-sourcemaps"),
+    concat = require("gulp-concat"),
+    less = require("gulp-less"),
+    notify = require("gulp-notify"),
+    uglify = require("gulp-uglify"),
+    jshint = require("gulp-jshint"),
+    jsStylish = require("jshint-stylish");
+
 
 gulp.task("default", function(){
-  gulp.watch("./src/less/**/*.less", ['css']);
-  gulp.watch(["./src/config/config.js",
-    "./src/exceptions/**/*.js",
-    "./src/models/**/*.js",
-    "./src/services/**/*.js",
-    "./src/viewmodels/**/*.js",
-    "./src/app.js"], ['js']);
+  gulp.watch("./public/less/**/*.less", ['css']);
+  gulp.watch(["./public/config/config.js",
+    "./public/exceptions/**/*.js",
+    "./public/models/**/*.js",
+    "./public/javascripts/services/**/*.js",
+    "./public/viewmodels/**/*.js",
+  ], ['js']);
 });
 gulp.task("js", function(){
-  gulp.src(["./src/config/config.js",
-    "./src/exceptions/**/*.js",
-    "./src/models/**/*.js",
-    "./src/services/**/*.js",
-    "./src/viewmodels/**/*.js",
-    "./src/app.js"])
-    .pipe(jshint())
-    .pipe(jshint.reporter(jsStylish))
-    .pipe(sourcemaps.init())
-    .pipe(concat("app.min.js"))
-    .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest("./src/dist/js"))
-    .pipe(notify({
-      message:"jsbuilt"
-    }));
+  gulp.src(["./public/config/config.js",
+    "./public/exceptions/**/*.js",
+    "./public/models/**/*.js",
+    "./public/javascripts/services/**/*.js",
+    "./public/javascripts/controllers/**/*.js",
+    "./public/viewmodels/**/*.js",
+    "./public//app.js"])
+      .pipe(jshint())
+      .pipe(jshint.reporter(jsStylish))
+      .pipe(sourcemaps.init())
+      .pipe(concat("app.min.js"))
+      .pipe(uglify())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest("./public/dist/js"))
+      .pipe(notify({
+        message:"jsbuilt"
+      }));
 
 });
 
 gulp.task("css", function(){
-  gulp.src("./src/less/**/*.less")
-    .pipe(less())
+  gulp.src("./public/stylesheets/**/*.less")
+      .pipe(less())
     /*.pipe(csslint({
      'ids': false
      }))
@@ -51,10 +53,10 @@ gulp.task("css", function(){
      .pipe(cssMinifier())
      .pipe(concat("site.css"))
      .pipe(sourcemaps.write())*/
-    .pipe(gulp.dest("./src/dist/css"))
-    .pipe(notify({
-      message:"css built"
-    }));
+      .pipe(gulp.dest("./public/dist/css"))
+      .pipe(notify({
+        message:"css built"
+      }));
 
   /**
    * sourcemaps -->
