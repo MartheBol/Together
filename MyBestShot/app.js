0f5981d0f27d7dded1387fb836fbd64a6ee91cb6
NikitaLisabeth <nikita.lisabeth@student.howest.be>
2015-12-17 14:49:38 +0100

    Werkende checkbox

---
+++ MyBestShot/app.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    "use strict";
+
+    var app = angular.module("app", []);
+    app.directive('ngEnter', function () {
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if (event.which === 13) {
+                    scope.$apply(function () {
+                        scope.$eval(attrs.ngEnter);
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    });
+
+    app.directive('editableCheckbox', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'editablecheckbox.html'
+        }
+    });
+
+})();
---
+++ MyBestShot/indexController.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    "use strict";
+
+    var indexController = function ($scope) {
+        $scope.categories = [''];
+        $scope.newCategory = "";
+        $scope.saveCategory = function () {
+
+        };
+
+    };
+
+    var editableCheckboxController = function ($scope) {
+        $scope.category = "";
+        $scope.showLabel = false;
+        $scope.showTextbox = true;
+        $scope.saveCategory = function () {
+            if ($scope.category) {
+                $scope.showLabel = true;
+                $scope.showTextbox = false;
+            }
+            else {
+                $scope.showLabel = false;
+                $scope.showTextbox = true;
+            }
+        };
+        $scope.edit = function () {
+            $scope.showLabel = false;
+            $scope.showTextbox = true;
+        }
+
+    };
+    angular.module("app")
+        .controller("indexController", ["$scope", indexController])
+        .controller("editableCheckboxController", ["$scope", editableCheckboxController]);
+
+
+})();
