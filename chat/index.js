b002f75dd46d6f1a3cf63fb2b32dc95423886647
NikitaLisabeth <nikita.lisabeth@student.howest.be>
2015-12-22 19:27:48 +0100

    Voorbeeld chat

---
+++ chat/index.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Marthe on 4/12/15.
+ */
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+// Initialize appication with route / (that means root of the application)
+app.get('/', function(req, res){
+    var express=require('express');
+    app.use(express.static(path.join(__dirname)));
+    res.sendFile(path.join(__dirname, '../chat2', 'index.html'));
+});
+
+// Register events on socket connection
+io.on('connection', function(socket){
+    socket.on('chatMessage', function(from, msg){
+        io.emit('chatMessage', from, msg);
+    });
+    socket.on('notifyUser', function(user){
+        io.emit('notifyUser', user);
+    });
+});
+
+// Listen application request on port 3000
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
