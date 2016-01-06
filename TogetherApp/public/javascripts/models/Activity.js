150647249e3560a66ce620bd9220086f3e667f85
Iman Cocquyt <iman.cocquyt@student.howest.be>
2016-01-06 22:58:20 +0100

    Dummy activities - indien er niet voldoende recente zijn in de db

--- TogetherApp/public/javascripts/models/Activity.js
+++ TogetherApp/public/javascripts/models/Activity.js
@@ -1,11 +1,15 @@
 /**
  * Created by iman on 15/12/15.
  */
-var Activity = function(img, name, place, keywords, participants, myEvent){
-    this.img = img;
-    this.name = name;
-    this.place = place;
-    this.keywords = keywords;
-    this.participants = participants;
-    this.myEvent = myEvent;
+var Activity = function(name, zipcode, street, number, description, dateFrom, dateUntil, timestamp, username, matches){
+    this.activityName = name;
+    this.zipcode = zipcode;
+    this.street = street;
+    this.number = number;
+    this.description = description;
+    this.dateFrom = dateFrom;
+    this.dateUntil = dateUntil;
+    this.timestamp = timestamp;
+    this.username = username;
+    this.matches = matches;
 };
