ee2d1b0294258c4c41c4ce3e9f7cf117381a01d1
MartheBol <Marthe@MacBook-Pro-van-Marthe.local>
2016-01-06 21:10:28 +0100

    Oplossing error substring

--- togetherApp/public/javascripts/controllers/ActivitiesController.js
+++ togetherApp/public/javascripts/controllers/ActivitiesController.js
@@ -15,16 +15,15 @@
                 var arrTemp = response.activitielist;
                 var arrActs = [];
 
-                var today = new Date().toISOString().substring(0, 10),
+                /*var today = new Date().toISOString().substring(0, 10),
                     todaySplit = today.split("-"),
-                    sumToday =  todaySplit[0] +  todaySplit[1] +  todaySplit[2];
+                    sumToday =  todaySplit[0] +  todaySplit[1] +  todaySplit[2];*/
 
                 for (var i = 0, l = arrTemp.length; i < l; i++) {
-                    var activtyDateSplit = arrTemp[i].untilDate.substring(0,10).split("-"),
+                    /*var activtyDateSplit = arrTemp[i].untilDate.substring(0,10).split("-"),
                         sumActivity =  activtyDateSplit[0] + activtyDateSplit[1] + activtyDateSplit[2];
-                    if ( sumActivity >= sumToday) {
+                    if ( sumActivity >= sumToday) {*/
                       arrActs.push(arrTemp[i]);
-                    }
                 }
 
                 $scope.arrActivities = arrActs;
