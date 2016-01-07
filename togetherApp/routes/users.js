d25874429496bbaebc282be90634072ff6174368
MartheBol <Marthe@MacBook-Pro-van-Marthe.local>
2016-01-07 15:10:41 +0100

    aanpassingen profile

--- togetherApp/routes/users.js
+++ togetherApp/routes/users.js
@@ -15,40 +15,39 @@ var activitySchema = require('../schemas/activitySchema.js');
 var activityCollection = mongoose.model('Activity', activitySchema, "activities");
 
 router.get('/', function (req, res) {
-  User.getUsers(function (users) {
-    res.json({userlist: users});
-  });
+    User.getUsers(function (users) {
+        res.json({userlist: users});
+    });
 });
 
-router.get('/userdelete/:username',find_correctuser, function (req, res) {
-  console.log('je komt in de userdelete route')
-  /*if(req.user === undefined || req.user.username !== 'admin'){
-    res.redirect('/#/home')
-  }
-  else {*/
-  userCollection.findOneAndUpdate(
-      {username: req.params.username},
-      {$set: {deleted : true}},
-      function(err, model)
-      {
-        console.log("dit is een" + err);
-      }
-  );
+router.get('/userdelete/:username', find_correctuser, function (req, res) {
+    console.log('je komt in de userdelete route')
+    /*if(req.user === undefined || req.user.username !== 'admin'){
+     res.redirect('/#/home')
+     }
+     else {*/
+    userCollection.findOneAndUpdate(
+        {username: req.params.username},
+        {$set: {deleted: true}},
+        function (err, model) {
+            console.log("dit is een" + err);
+        }
+    );
 
-     Activity.getActivities(function (activities) {
-        for(var i = 0; i<activities.length; i++){
-            for(var ii = 0; ii<activities[i].matches.length; ii++) {
-                if(activities[i].matches[ii] == req.params.username){
+    Activity.getActivities(function (activities) {
+        for (var i = 0; i < activities.length; i++) {
+            for (var ii = 0; ii < activities[i].matches.length; ii++) {
+                if (activities[i].matches[ii] == req.params.username) {
                     var matchactivities = [];
                     matchactivities.push(activities[i]);
 
-                    for(var iii = 0; iii<matchactivities.length; iii++){
+                    for (var iii = 0; iii < matchactivities.length; iii++) {
                         console.log(matchactivities[iii].activityName)
                         activityCollection.findOneAndUpdate(
                             {activityName: matchactivities[iii].activityName},
                             {$pull: {matches: req.params.username}},
-                            function(err, model) {
-                                console.log("dit is een "+err);
+                            function (err, model) {
+                                console.log("dit is een " + err);
 
                             }
                         );
@@ -62,20 +61,38 @@ router.get('/userdelete/:username',find_correctuser, function (req, res) {
 
 });
 
+router.get('/userdetail/:username', find_correctuser, function (req, res) {
+    console.log(req.params.username);
 
-router.get('/userdetail/:username',find_correctuser, function (req, res) {
-  console.log(req.params.username);
+    if (req.user === undefined || req.user.username !== 'admin') {
+        res.redirect('/#/home')
 
-  if(req.user === undefined || req.user.username !== 'admin'){
-    res.redirect('/#/home')
+    }
+    else {
+        res.json({correctuser: req.correctuser});
+        //res.render('/#/userdetails', {correctuser: req.correctuser});
+        //res.redirect('/#/userdetails');
+    }
 
-  }
-  else{
-    res.json({correctuser: req.correctuser});
-    //res.render('/#/userdetails', {correctuser: req.correctuser});
-    //res.redirect('/#/userdetails');
-  }
+});
 
+router.get('/updateprofile', function (res, req) {
+    userCollection.findOneAndUpdate(
+        {username: req.user.username},
+        {
+            $set: {
+                username: req.body.username,
+                password: req.body.password,
+                zipcode: req.body.zipcode,
+                birthdate : req.body.birthdate,
+                sex : req.body.sex,
+                biography: req.body.biography
+            }
+        },
+        function (err, model) {
+            console.log("dit is een" + err);
+        }
+    );
 });
 
 module.exports = router;
