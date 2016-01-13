54a615a47968a14600b3e1c2197cd981f6d2ca48
Iman Cocquyt <iman.cocquyt@student.howest.be>
2016-01-13 15:01:55 +0100

    Validation signup + consistente weergave datums

--- togetherApp/public/javascripts/controllers/loginController.js
+++ togetherApp/public/javascripts/controllers/loginController.js
@@ -26,7 +26,7 @@
                 });
         };
 
-        $scope.interests = ['Jazz', 'Hiphop', 'New wave', 'Traveling','Party'];
+        $scope.interests = ['Music', 'Traveling'];
         $scope.lst = [];
         $scope.change = function(){
             $scope.lst.push('2');
@@ -83,26 +83,104 @@
             }
         };
 
+        //http://stackoverflow.com/questions/5812220/how-to-validate-a-date
+        function isValidDate(d, m, y) {
+            // Assume not leap year by default (note zero index for Jan)
+            var daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+            // If evenly divisible by 4 and not evenly divisible by 100,
+            // or is evenly divisible by 400, then a leap year
+            if( (y%100 && !y%4) || (!y%400)){
+                daysInMonth[1] = 29;
+            }
+
+            /*
+            if ( ((!(y % 4)) && y % 100) || (!(y % 400))) {
+                daysInMonth[1] = 29;
+            }
+            */
+            return d <= daysInMonth[--m];
+        }
+
+        function isRequiredField(element) {
+            if (element) {
+                if (element.value === '' || element === undefined) {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+
+        function isValidEmail(email) {
+            var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return pattern.test(email);
+        }
+
+        function isValidZipcode(zipcode) {
+            var pattern = /^[1-9]{1}[0-9]{3}$/;
+            return pattern.test(zipcode);
+        }
+
         $scope.register = function() {
 
-            $http.post('/register', {
-                firstname: this.firstname,
-                lastname : this.lastname,
-                //email : this.email,*/
-                username : this.username,
-                password : this.password,
-                zipcode: this.zipcode,
-                birthdate : this.birthdate,
-                sex : this.sex,
-                biography : this.biography,
-                geolocation:$scope.geolocation,
-                interests:selectedInterest
+            if(isRequiredField(this.firstname) &&
+                isRequiredField(this.lastname) &&
+                isRequiredField(this.password) &&
+                isRequiredField(this.sex) &&
+                isRequiredField(this.biography)){
 
-            }).success(function (data) {
-                $scope.error = data.error;
-                $location.path(data.redirect);
-            });
+                $scope.error = "";
+                if(isValidDate(this.birthdateDay, this.birthdateMonth, this.birthdateYear)){
 
+                    var birthdate = this.birthdateYear + "-" + this.birthdateMonth + "-" + this.birthdateDay;
+
+                    $scope.error = "";
+                    if(isValidZipcode(this.zipcode)){
+
+                        $scope.error = "";
+                        if(isValidEmail(this.username)){
+
+                            $scope.error = "";
+                            $http.post('/register', {
+                                firstname: this.firstname,
+                                lastname : this.lastname,
+                                //email : this.email,
+                                username : this.username,
+                                password : this.password,
+                                zipcode: this.zipcode,
+                                //birthdate : this.birthdate,
+                                birthdate: birthdate,
+                                sex : this.sex,
+                                biography : this.biography,
+                                geolocation:$scope.geolocation,
+                                interests:selectedInterest
+
+                            }).success(function (data) {
+                                $scope.error = data.error;
+                                $location.path(data.redirect);
+                            });
+
+                        }
+                        else {
+
+                            $scope.error = "Please enter a valid email as username.";
+
+                        }
+                    }
+                    else{
+                        $scope.error = "Please enter a valid Belgian postcode.";
+                    }
+                }
+                else{
+
+                    $scope.error = "Please enter a valid date as birthdate.";
+                }
+            }
+            else{
+                $scope.error = "All fields are required when making a new account.";
+            }
         };
     };
 
